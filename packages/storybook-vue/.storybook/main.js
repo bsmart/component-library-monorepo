26e852c67897482331a2b0136c53fb2187d556bb
bsmart <2787779+bsmart@users.noreply.github.com>
2021-06-14 22:59:38 -0700

    Add postcss addon

--- packages/storybook-vue/.storybook/main.js
+++ packages/storybook-vue/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,6 +8,34 @@ module.exports = {
 	"addons": [
 		"@storybook/addon-a11y",
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+		"@storybook/addon-essentials",
+		"@storybook/addon-postcss"
+	],
+  webpackFinal: async (config, { configType }) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+      '@': path.resolve(__dirname, "../src")
+    }
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            }
+
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  }
 }
